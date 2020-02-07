b7d453e9ae6296335999f5e6e39b20a88f1e327b
cdefl0 <60612704+cdefl0@users.noreply.github.com>
2020-02-07 08:42:51 -0600

    Add files via upload

---
+++ HelloGithub.py
@@ -0,0 +1,68 @@
+# comment
+
+print ("Hello, World!")
+a = 6
+b = 5
+print (a + b)
+
+#how to make a list
+
+myList = [1, 2, 3]
+
+#how to access elements of a list
+
+print (myList[1])
+
+#how to write a function
+
+def sumFunction(a, b):
+	return a + b
+
+#how to call a function
+
+print (sumFunction(2, 20))
+
+x = 10
+y = -10
+
+#blocks of code
+
+if (x == 10):
+	x = 5
+
+elif (y == -10):
+	y = 5
+
+else: 
+	x = y - x
+
+#loop
+
+myList = [1, 3, 8, 412, 43, 2, 20]
+
+#length of list
+
+len(myList)
+
+#for loop by index
+
+for i in range (len(myList)):
+	print(myList[i])
+
+#for each loop
+
+for val in myList:
+	print (myList[i])
+
+#dictionaries
+
+lookup = {}
+lookup ['kc'] = 'chiefs'
+lookup ['ne'] = 'patriots'
+lookup ['la'] = 'chargers'
+lookup ['suck'] = 'raiders'
+
+#while loop
+
+while x < 10:
+        x++
